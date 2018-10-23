3d457fc52a3137d9ec8622e77afafaa0f21423f1
veraposeidon <veraposeidon@gmail.com>
2018-10-23 01:28:20 +0000

    完成了第三节 高级篇

--- tutorials/03-advanced/image_captioning/download.sh
+++ tutorials/03-advanced/image_captioning/download.sh
@@ -1,5 +1,5 @@
 mkdir data
-wget http://msvocds.blob.core.windows.net/annotations-1-0-3/captions_train-val2014.zip -P ./data/
+#wget http://msvocds.blob.core.windows.net/annotations-1-0-3/captions_train-val2014.zip -P ./data/
 wget http://images.cocodataset.org/zips/train2014.zip -P ./data/
 wget http://images.cocodataset.org/zips/val2014.zip -P ./data/
 
